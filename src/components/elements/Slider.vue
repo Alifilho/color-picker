519f4711c84cbcc114bfa3bfda807ebb721b2307
Alisson Oliveira <alissonoliveiram@gmail.com>
2020-10-30 00:34:43 -0300

    first commit

---
+++ src/components/elements/Slider.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="slider">
+        <input type="range" min="0" max="255" v-model="value">
+    </div>
+</template>
+
+<script>
+
+
+export default {
+    name: 'Slider',
+    props: {
+        propValue: Number,
+    },
+    created() {
+        this.value = this.propValue
+    },
+    data: () => ({
+        value: 0
+    }),
+    watch: {
+        value() {
+            this.$emit('rgb', this.value)
+        }
+    }
+    
+}
+</script>
+
+<style scoped>
+    .slider {
+        
+    }
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <Slider :value="r" @rgb="changeColor"/>
+    <Slider :value="g" @rgb="changeColor"/>
+    <Slider :value="b" @rgb="changeColor"/>
+
+    <p>R: {{r}}</p>
+    <p>G: {{g}}</p>
+    <p>B: {{b}}</p>
+  </div>
+</template>
+
+<script>
+import Slider from '../components/elements/Slider'
+
+export default {
+  name: 'Home',
+  components: {
+    Slider
+  },
+  data: () => ({
+    r: 125,
+    g: 0,
+    b: 255
+  }),
+  methods: {
+    changeColor: (color) => {
+      this.r = color;
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
